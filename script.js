3dd4d98847918fa1bab5858d1f72a0f365d9afdc
Oberth Zolán Zsigmond <oberthzoltan@4ig.hu>
2019-03-11 08:01:23 +0100

    .

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Set map view
 let map = L.map("map").setView([47.529349, 19.032751], 11);
+
+// if zoom repositioning svg
+map.on("zoom", function() {
+  var svgContainer = d3.select(map.getPanes().overlayPane).selectAll("svg");
+  var svg = svgContainer.selectAll("svg");
+  positionSvgContainer();
+  setPosition(svg);
+});
+
+//console.log click latlng + pixel points
 map.on("click", function(e) {
   var coord = e.latlng;
   var lat = coord.lat;
@@ -14,26 +24,21 @@ map.on("click", function(e) {
       xy
   );
 });
-map.on("zoom", function() {
-  var svgContainer = d3.select(map.getPanes().overlayPane).select("svg");
-  var g = svgContainer.selectAll("g");
-  positionSvgContainer();
-  setPosition(g);
-});
 
+// set svg container position to leaflet-map-pane position
 function positionSvgContainer() {
-  var svgContainer = d3.select(map.getPanes().overlayPane).select("svg");
+  var svgContainer = d3.select(map.getPanes().overlayPane).selectAll("svg");
   var tr = d3
     .selectAll(".leaflet-map-pane")
     .style("transform")
     .split(",");
-  console.log(tr);
   var tx = -1 * tr[0].match(/-*\d+\.*\d*px/)[0].match(/-*\d+\.*\d*/)[0];
   var ty = -1 * tr[1].match(/-*\d+\.*\d*px/)[0].match(/-*\d+\.*\d*/)[0];
   var height = svgContainer.attr("height");
   var width = svgContainer.attr("width");
   svgContainer.style(
     "transform",
+    "scale(0.08, 0.08)",
     "translate3d(" + tx + "px, " + ty + "px, 0px)"
   );
   svgContainer.attr("viewBox", tx + " " + ty + " " + width + " " + height);
@@ -43,10 +48,11 @@ function positionSvgContainer() {
 let marker1 = L.marker([47.529349, 19.032751]).addTo(map);
 let marker2 = L.marker([47.52936, 19.03276]).addTo(map);
 
+//sensor markers
 let sensor1;
 let sensor2;
 let sensor3;
-
+//sensor icon
 let sensorIcon = L.icon({
   iconUrl: "./img/sensor-icon.png",
   iconSize: [32, 32],
@@ -57,7 +63,6 @@ let sensorIcon = L.icon({
 setInterval(() => {
   getMarkerData();
 }, 100);
-
 getSensorData();
 
 //set maps layer
@@ -65,12 +70,6 @@ L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-//set maps default bounds
-// map.fitBounds([
-//     [sensor1.getBounds()],
-//     [sensor2.getBounds()]
-// ], );
 
 //Get marker data from server
 function getMarkerData() {
@@ -170,6 +169,7 @@ function getSensorData() {
   xml.onload = () => {
     if (xml.status === 200) {
       let sensorData = JSON.parse(xml.responseText).sensors;
+
       let sensorsLatLon = [];
       let coords = sensorData.map(data => Object(data.domain.cordinate));
 
@@ -180,9 +180,6 @@ function getSensorData() {
       sensor1 = L.marker(sensorsLatLon[0], {
         icon: sensorIcon
       }).addTo(map);
-
-      pixelPosition = map.latLngToLayerPoint(sensorsLatLon[1]);
-      //console.log(" LatLng = " + sensorsLatLon[1] + "\n Pixel position = " + pixelPosition);
 
       sensor2 = L.marker(sensorsLatLon[1], {
         icon: sensorIcon
@@ -207,6 +204,8 @@ function getSensorData() {
     llMarkers[k].classList.add("droneMarkerIcon");
   }
 }
+
+//create circular sector svg from sensor data
 
 function elemikorcikk(rmin, rmax, minAng, maxAng, x, y) {
   x = 0;
@@ -235,7 +234,7 @@ function elemikorcikk(rmin, rmax, minAng, maxAng, x, y) {
     endy
   ].join(" ");
   element.width = (rmax - rmin) / 2;
-  //console.log(element)
+  //console.log(rmin, rmax, element.d);
   return element;
 }
 
@@ -243,57 +242,59 @@ function interpol(min, max, n, i) {
   return min + (i * (max - min)) / n;
 }
 
+//Create all mrSvgs->groups->paths
+
 function igazi(sensorData, n) {
-  //for (i = 0; i < n; i++) {
-  var svgContainer = d3.select(map.getPanes().overlayPane).append("svg");
-  d3.select("svg")
+  var svgContainer = d3.select(map.getPanes().overlayPane);
+
+  var svg = svgContainer.selectAll("svg").data(sensorData, d => {
+    return d.id;
+  });
+
+  svg.exit().remove();
+  var newSvg = svg.enter().append("svg");
+  svg = newSvg.merge(svg);
+
+  svg
     .attr("z-index", 1000)
     .attr("height", 686)
     .attr("width", 1162)
     .attr("id", "mr");
 
-  var g = svgContainer.selectAll("g").data(sensorData, d => {
-    return d.id;
-  });
-  //console.log(map.latLngToLayerPoint((map.getSize())));
-  g.exit().remove();
-  let newG = g.enter().append("g");
-  g = newG.merge(g);
-
-  setPosition(g);
-  // g.attr("class", "svgG")
-  //     .attr("transform-origin", function (d) {
-  //         var sensorLL = [d.domain.cordinate.latitude, d.domain.cordinate.longitude];
-  //         //console.log(map.latLngToLayerPoint(sensorLL))
-  //         return (map.latLngToLayerPoint(sensorLL).x) + " " + (map.latLngToLayerPoint(sensorLL).y);
-  //     })
-  //     .attr("transform", function (d) {
-  //         var sensorLL = [d.domain.cordinate.latitude, d.domain.cordinate.longitude];
-  //         //console.log(map.latLngToLayerPoint(sensorLL))
-  //         return "scale(0.1, 0.1), translate(" + (map.latLngToLayerPoint(sensorLL).x) + " " + (map.latLngToLayerPoint(sensorLL).y) + ")";
-  //     });
-  for (i = 0; i < n; i++) {
-    var path = g
-      .append("path")
+  svg.each(function(d, i) {
+    var path = d3
+      .select(this)
+      .selectAll("path")
+      .data(new Array(n));
+    path.exit().remove();
+    let newPath = path.enter().append("path");
+    path = newPath.merge(path);
+    path
       .attr("fill", "none")
       .attr("stroke", "red")
       .attr("opacity", "0.1")
-      .attr("stroke-width", 500 + i)
-      .attr("d", function(d) {
-        //console.log(getSensorPath(d.domain, n, i))
+      .attr("stroke-width", function(d2, i) {
+        //console.log(sensorData);
+        return getSensorPathWidth(d.domain, n, i);
+      })
+      .attr("d", function(d2, i) {
         return getSensorPath(d.domain, n, i);
       });
-  }
+  });
 }
-//}
-function setPosition(g) {
-  g.attr("class", "svgG")
+// set mrSvg position to sensor marker
+
+function setPosition(svg) {
+  var svgContainer = d3.select(map.getPanes().overlayPane).selectAll("svg");
+  console.log(svgContainer);
+  svgContainer
+    //.attr("class", "svgSVG")
     .attr("transform-origin", function(d) {
       var sensorLL = [
         d.domain.cordinate.latitude,
         d.domain.cordinate.longitude
       ];
-      //console.log(map.latLngToLayerPoint(sensorLL))
+      //console.log(map.latLngToLayerPoint(sensorLL));
       return (
         map.latLngToLayerPoint(sensorLL).x +
         " " +
@@ -305,7 +306,7 @@ function setPosition(g) {
         d.domain.cordinate.latitude,
         d.domain.cordinate.longitude
       ];
-      //console.log(map.latLngToLayerPoint(sensorLL))
+      //console.log(map.latLngToLayerPoint(sensorLL));
       return (
         "scale(0.08, 0.08), translate(" +
         map.latLngToLayerPoint(sensorLL).x +
@@ -315,6 +316,8 @@ function setPosition(g) {
       );
     });
 }
+
+//calculate svg paths from sensor data
 
 function getSensorPath(domain, n, i) {
   var rmin0 = domain.r.min0 * Math.cos(domain.theta.min0);
@@ -335,25 +338,15 @@ function getSensorPath(domain, n, i) {
     0,
     0
   );
-
+  //console.log(element1.d);
   return element1.d;
 }
-
-// function resetView() {
-//   var svgContainer = d3.select(map.getPanes().overlayPane).select("svg");
-//   console.log(svgContainer);
-//   bounds = [
-//     [map.getBounds()._southWest.lat, map.getBounds()._southWest.lng],
-//     [map.getBounds()._northEast.lat, map.getBounds()._northEast.lng]
-//   ];
-
-//   var bottomLeft = map.project(bounds[0]);
-//   var topRight = map.project(bounds[1]);
-
-//   svgContainer
-//     .attr("width", topRight.x - bottomLeft.x)
-//     .attr("height", bottomLeft.y - topRight.y)
-//     .attr("transform", "translate(" + -bottomLeft.x + "," + -topRight.y + ")")
-//     .style("margin-left", bottomLeft.x + "px")
-//     .style("margin-top", topRight.y + "px");
-// }
+function getSensorPathWidth(domain, n, i) {
+  var rmin0 = domain.r.min0 * Math.cos(domain.theta.min0);
+  var rmin1 = domain.r.min1 * Math.cos(domain.theta.min0);
+  var rmax0 = domain.r.max0 * Math.cos(domain.theta.min0);
+  var rmax1 = domain.r.max1 * Math.cos(domain.theta.min0);
+  var width = interpol(rmax0, rmax1, n, i) - interpol(rmin0, rmin1, n, i);
+  //console.log(n, i, rmin0, rmin1, rmax0, rmax1, width);
+  return width;
+}
